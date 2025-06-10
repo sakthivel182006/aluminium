578a75ced47c8642385a8571a8f707cbac3af376
sakthivel182006 <156519241+sakthivel182006@users.noreply.github.com>
2025-06-11 02:45:14 +0530

    Update bookingcontroller.js

--- backend/controller/bookingcontroller.js
+++ backend/controller/bookingcontroller.js
@@ -134,7 +134,7 @@ export const deleteBooking = async (req, res) => {
     res.status(500).json({ message: "Server error. Unable to delete booking." });
   }
 };
-export const deleteAllBookings = async (req, res) => {
+export const deleteallbookings = async (req, res) => {
   try {
     const result = await Booking.deleteMany({});
     res.status(200).json({
